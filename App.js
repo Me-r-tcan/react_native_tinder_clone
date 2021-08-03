e5cabf17698931258da48602114ce0ed220430eb
Mertcan baş <mertcanbas35@gmail.com>
2021-08-04 01:46:06 +0300

    Add card touch animation.

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+} from 'react-native-reanimated';
+import {PanGestureHandler} from 'react-native-gesture-handler';
 
 import TinderCard from './src/components/TinderCard';
 import users from './assets/data/users';
 
 const App = () => {
+  const translateX = useSharedValue(0);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+    ],
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: () => {
+      console.log('Touch End');
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <TinderCard user={users[1]} />
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+          <TinderCard user={users[1]} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
@@ -17,6 +49,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  animatedCard: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
