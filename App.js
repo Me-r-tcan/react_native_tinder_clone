7b07a7eed32ee97d37a19a96a6fa0c5e1c5fa90f
Me-r-tcan <mertcanbas35@gmail.com>
2021-08-04 22:02:38 +0300

    Add the other tinder card.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import Animated, {
   useSharedValue,
@@ -15,6 +15,12 @@ import users from './assets/data/users';
 const ROTATION = 60;
 
 const App = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
+
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
+
   const {width: screenWidth} = useWindowDimensions();
 
   const hiddenTranslateX = 2 * screenWidth;
@@ -22,7 +28,7 @@ const App = () => {
   const translateX = useSharedValue(0);
   const rotate = useDerivedValue(
     () =>
-      interpolate(translateX.value, [0, -hiddenTranslateX], [0, ROTATION]) +
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
       'deg',
   );
 
@@ -35,6 +41,23 @@ const App = () => {
         rotate: rotate.value,
       },
     ],
+  }));
+
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.8, 1],
+        ),
+      },
+    ],
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.8, 1],
+    ),
   }));
 
   const gestureHandler = useAnimatedGestureHandler({
@@ -51,9 +74,15 @@ const App = () => {
 
   return (
     <View style={styles.container}>
+      <View style={styles.nextCardContainer}>
+        <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+          <TinderCard user={nextProfile} />
+        </Animated.View>
+      </View>
+
       <PanGestureHandler onGestureEvent={gestureHandler}>
         <Animated.View style={[styles.animatedCard, cardStyle]}>
-          <TinderCard user={users[1]} />
+          <TinderCard user={currentProfile} />
         </Animated.View>
       </PanGestureHandler>
     </View>
@@ -67,9 +96,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   animatedCard: {
+    flex: 1,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  nextCardContainer: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
   },
 });
 
